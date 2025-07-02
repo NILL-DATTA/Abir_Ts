b40d5d2e54bade844919d955e134dda59fd48b0f
kayalraju <87748199+kayalraju@users.noreply.github.com>
2025-07-02 12:33:14 +0530

    dd

---
+++ store/store.ts
@@ -0,0 +1,31 @@
+import { create } from "zustand";
+import { Cookies } from "react-cookie";
+
+interface UserState {
+  token: string | null;
+  logout: () => void;
+  setTokenAndUser: (token: string) => void;
+}
+const cookie = new Cookies();
+const useUserStore = create<UserState>((set) => ({
+  token: cookie.get("Token") || null,
+  user: null,
+  name: null,
+
+  setTokenAndUser: (token: string, user?: any) => {
+    if (token) {
+      cookie.set("Token", token, {
+        path: "/",
+        secure: true,
+      });
+      set({ token });
+    }
+  },
+
+  logout: () => {
+    cookie.remove("Token", { path: "/" });
+    set({ token: null });
+  },
+}));
+
+export default useUserStore;
